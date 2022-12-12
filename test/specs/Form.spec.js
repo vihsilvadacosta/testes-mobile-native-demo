e88aee0d5169f9194445f2d604775c27be581228
vihsilvadacosta <vitoria1.silva.costa@gmail.com>
2022-12-12 14:37:27 -0300

    adicionando validacao

--- test/specs/Form.spec.js
+++ test/specs/Form.spec.js
@@ -3,6 +3,7 @@ const homeScreen = require("../screens/home.screen");
 describe('Access Form Panel', ()=>{
     it('Should fill form', async() => {
         await homeScreen.goToForms()
+        expect(await $('~text-input')).toBeDisplayed()
         await homeScreen.insertText()
         await homeScreen.selectDropdown()      
         await homeScreen.selectingSwitch() 
